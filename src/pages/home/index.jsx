8e16c0146c8bf904b6a5be242c12d8f5d19b8db8
vikas2705 <vikas.vikasranjan@gmail.com>
2022-11-06 11:49:44 +0530

    carousel - 6 nov

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../../common/components/header";
 import NavbarMain from "../../common/components/navbar-main";
+import ImageSlider from "./components/image-slider";
 import "./home.css";
 
 const Home = () => {
@@ -8,9 +9,7 @@ const Home = () => {
         <div className='home-page'>
             <Header />
             <NavbarMain />
-
-            <section className='common-section'>Image slider</section>
-
+            <ImageSlider />
             <section className='common-section'>Popular offers</section>
 
             <section className='common-section'>Payments</section>
