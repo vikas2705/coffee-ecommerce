0838f9b66ad5839e2d2a7949517200f1f3579a0b
vikas2705 <vikas.vikasranjan@gmail.com>
2022-11-12 21:40:52 +0530

    all changes for footer bottom

--- src/common/components/footer-bottom/index.jsx
+++ src/common/components/footer-bottom/index.jsx
@@ -1,7 +1,72 @@
 import React from "react";
+import "./footer-bottom.css";
+import { footerLinks } from "./constants/footerOptions";
+import PaymentIcons from "../../assets/images/payment-icons.svg";
+import AppDownload from "../../assets/images/app-download.svg";
+import Facebook from "../../assets/images/facebook.svg";
+import Twitter from "../../assets/images/twitter.svg";
+import Youtube from "../../assets/images/youtube.svg";
+import Instagram from "../../assets/images/instagram.svg";
 
 const FooterBottom = () => {
-    return <section className='footer-bottom-main'>Footer Bottom</section>;
+    return (
+        <section className='footer-bottom-main'>
+            <div className='container'>
+                <div className='row'>
+                    <div className='left-main col-md-9'>
+                        <div className='left-top'>
+                            <div className='row'>
+                                {footerLinks.map(footerLink => {
+                                    return (
+                                        <div className='col-md-3'>
+                                            <h2 className='footer-list-heading'>
+                                                {footerLink.heading}
+                                            </h2>
+                                            <ul>
+                                                {footerLink.items.map(item => {
+                                                    return (
+                                                        <li className='footer-list-item'>
+                                                            <a href='#/'>
+                                                                {item}
+                                                            </a>
+                                                        </li>
+                                                    );
+                                                })}
+                                            </ul>
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        </div>
+                        <div className='left-bottom'>
+                            <img src={PaymentIcons} alt='all payment icons' />
+                        </div>
+                    </div>
+                    <div className='right-main col-md-3'>
+                        <div className='right-top'>
+                            <h3 className='footer-right-subheading'>
+                                Download our amazing app
+                            </h3>
+                            <div className='footer-right-icons'>
+                                <img src={AppDownload} alt='download apps' />
+                            </div>
+                        </div>
+                        <div className='right-bottom'>
+                            <h3 className='footer-right-subheading'>
+                                Connect us on
+                            </h3>
+                            <div className='footer-right-icons'>
+                                <img src={Facebook} alt='Facebook icon' />
+                                <img src={Twitter} alt='Twitter icon' />
+                                <img src={Youtube} alt='Youtube icon' />
+                                <img src={Instagram} alt='Instagram icon' />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default FooterBottom;
--- src/common/components/footer-top/index.jsx
+++ src/common/components/footer-top/index.jsx
@@ -1,34 +1,6 @@
 import React from "react";
 import "./footer-top.css";
-import Wallet from "../../assets/images/wallet.svg";
-import SecurePay from "../../assets/images/secure-pay.svg";
-import Contact from "../../assets/images/contact.svg";
-import Calculator from "../../assets/images/calculator.svg";
-
-const features = [
-    {
-        icon: Wallet,
-        title: "100% Secure Payment",
-        subTitle: "Moving your card details to a much more secured place",
-    },
-    {
-        icon: SecurePay,
-        title: "Trust pay",
-        subTitle: "Payment Protection. Easy Return Policy",
-    },
-    {
-        icon: Contact,
-        title: "Help Center",
-        subTitle:
-            " Look no further. Browse our FAQs or submit your query here.",
-    },
-    {
-        icon: Calculator,
-        title: "Easy Installment",
-        subTitle:
-            "Pay for your purchase in Easy EMI at very low interest rates",
-    },
-];
+import { features } from "./constants/features";
 
 const FooterTop = () => {
     return (
